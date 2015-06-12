e26506a7cf63329f9f3a0d335839b180ee839010
DavidAbel <drabelpdx@gmail.com>
2015-06-12 10:52:55 -0700

    Can delete and edit a question.

--- controllers/question.js
+++ controllers/question.js
@@ -1,3 +1,22 @@
 App.QuestionController = Ember.ObjectController.extend({
+  needs: ['question'],
+  isEditing: false,
+  actions: {
+    edit: function() {
+      this.set('isEditing', true);
+    },
+    save: function() {
+      this.set('isEditing', false);
+      var asked = this.get('model');
+      var question = this.get('controllers.question.model');
+      asked.save();
+    },
+    delete: function() {
+      if (confirm('Are you sure?')) {
+        this.get('model').destroyRecord();
+        this.transitionToRoute('questions');
+      }
+    }
+  }
 
 });
