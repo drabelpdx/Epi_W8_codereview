604126f4f5580e5dbdd1ad2b8299c65c022d8c14
DavidAbel <drabelpdx@gmail.com>
2015-06-12 10:39:45 -0700

    Moved questions page to root.

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 App.Router.map(function(){
-  this.resource('questions');
+  this.resource('questions', {path: '/'});
   this.resource('new-question');
   this.resource('question', {path: '/questions/:question_id'});
 
